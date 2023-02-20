0f67a76df5ccf27cc76f38a63f6a921f2c158c78
killian31 <killian.steunou@gmail.com>
2023-02-20 11:00:00 +0100

    Initial commit

---
+++ images_to_video.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import os
+from tqdm import tqdm
+
+class VideoCreator:
+    def __init__(self, imgs_dir, vid_name):
+        self.imgs_dir = imgs_dir
+        self.img_array = []
+        self.video_filename = vid_name
+
+    def preprocess_images(self):
+        filenames = sorted(os.listdir(self.imgs_dir))
+        print("Adding images...")
+        for filename in tqdm(filenames):
+            complete_filename = self.imgs_dir + "/" + filename
+            img = cv2.imread(complete_filename)
+            height, width, _ = img.shape
+            size = (width, height)
+            self.img_array.append(img)
+
+        return size
+
+
+    def create_video(self):
+        size = self.preprocess_images()
+        out = cv2.VideoWriter(self.video_filename, cv2.VideoWriter_fourcc(*"MJPG"), 20, size)
+        print("Recording video...")
+        for i in tqdm(range(len(self.img_array))):
+            out.write(self.img_array[i])
+        out.release()
+        print("Done.")
+
+if __name__ == "__main__":
+    creator = VideoCreator("/data3/airbus/onboard_vid/person0.05", "test_vid.avi")
+    creator.create_video()
---
+++ video_to_images.py
@@ -0,0 +1,45 @@
+import cv2
+from tqdm import tqdm
+import os
+
+class ImageCreator:
+    def __init__(self, filename, imgs_dir, image_start=0, image_end=0):
+        """
+        :param str filename: The name of the video's filename.
+        :param str imgs_dir: The directory where to store the image files.
+        :param int image_start: The first image to be exctracted.
+        :param int image_end: The last image to be extracted, 0 if full video.
+        """
+
+        self.filename = filename
+        self.imgs_dir = imgs_dir
+        self.image_start = image_start
+        self.image_end = image_end
+        if not os.path.exists(imgs_dir):                                           
+            os.makedirs(imgs_dir)
+
+    def get_images(self):
+        vid = cv2.VideoCapture(self.filename)
+        total_frames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+        success, image = vid.read()
+        count = 0
+        image_start = self.image_start
+        if self.image_end == 0:
+            self.image_end = total_frames
+        zfill_max = len(str(total_frames))
+        ok_count = 0
+        pbar = tqdm(total=total_frames)
+
+        while success:
+            if count >= self.image_start and count <= self.image_end:
+                cv2.imwrite(f"{self.imgs_dir}/frame_{str(ok_count).zfill(zfill_max)}.jpg", image) 
+                ok_count += 1
+            success, image = vid.read()
+            pbar.update(1)
+            count += 1
+        pbar.close()
+        print("Wrote {} image files.".format(ok_count))
+
+if __name__ == "__main__":
+    vid_to_im = ImageCreator("Video_embarquement_1.avi", "testF", image_start=400, image_end=2743)
+    vid_to_im.get_images()
